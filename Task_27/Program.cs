4bfd292158ec0a2484eb777094695dabea111cd3
Alex <alexchurilov@mail.ru>
2022-05-26 17:15:49 +0300

    Add Tasks 27, 29

---
+++ Task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу, которая принимает на вход число и выдает сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+  int FigSum(int number)
+  {
+    int count = Convert.ToString(number).Length;
+    int adv = 0;
+    int res = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      adv = number - number % 10;
+      res = res + (number - adv);
+      number = number / 10;
+    }
+   return res;
+  }
+
+int figSum = FigSum(number);
+Console.WriteLine($"Сумма цифр в числе: {figSum}");
